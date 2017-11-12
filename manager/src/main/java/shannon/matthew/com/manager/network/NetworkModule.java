85ca0555d00bd81b28d130958a80f4a47a3a198f
Matthew Shannon <shanno29@uwm.edu>
2017-11-11 18:17:03 -0600

    added the ability for users of the library to define their own network interceptors

--- manager/src/main/java/shannon/matthew/com/manager/network/NetworkModule.java
+++ manager/src/main/java/shannon/matthew/com/manager/network/NetworkModule.java
@@ -4,19 +4,25 @@ import android.content.Context;
 
 import com.google.gson.Gson;
 
+import java.util.Set;
+
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import java9.util.stream.StreamSupport;
 import okhttp3.Cache;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static java9.util.stream.StreamSupport.*;
 import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
 
 @Module
@@ -32,21 +38,16 @@ public abstract class NetworkModule {
 
   @Provides
   @Singleton
-  static HttpLoggingInterceptor loggingInterceptor() {
-    return new HttpLoggingInterceptor().setLevel(BODY);
-  }
-
-  @Provides
-  @Singleton
   static Cache provideOkHttpCache(Context context) {
     return new Cache(context.getCacheDir(), 10 * 1024 * 1024);
   }
 
   @Provides
   @Singleton
-  static OkHttpClient provideOkHttpClient(Cache cache, HttpLoggingInterceptor interceptor) {
-    return new OkHttpClient.Builder()
-      .addInterceptor(interceptor)
+  static OkHttpClient provideOkHttpClient(Cache cache, Set<Interceptor> interceptors) {
+    OkHttpClient.Builder builder = new OkHttpClient.Builder();
+    stream(interceptors).forEach(builder::addInterceptor);
+    return builder
       .connectTimeout(30, SECONDS)
       .writeTimeout(30, SECONDS)
       .readTimeout(30, SECONDS)
