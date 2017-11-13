a442e1529dd1dfe10014e6f62b72d54ffa286353
Matthew Shannon <shanno29@uwm.edu>
2017-11-12 19:32:19 -0600

    Cleanup

--- manager/src/main/java/shannon/matthew/com/manager/prefs/PrefsModule.java
+++ manager/src/main/java/shannon/matthew/com/manager/prefs/PrefsModule.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.f2prateek.rx.preferences2.RxSharedPreferences;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -13,8 +17,15 @@ public abstract class PrefsModule {
   public PrefsModule() {}
 
   @Provides
+  @Singleton
   static SharedPreferences sharedPreferences(Context context) {
     return PreferenceManager.getDefaultSharedPreferences(context);
+  }
+
+  @Provides
+  @Singleton
+  static RxSharedPreferences rxPreferences(SharedPreferences preferences) {
+    return RxSharedPreferences.create(preferences);
   }
 
 }
